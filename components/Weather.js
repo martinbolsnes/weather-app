9c0bc3b01f773d8ca6a8e9592908879b11078375
martinbolsnes <martinbolsnes@icloud.com>
2022-12-21 13:31:20 +0100

    add roundedtemp & weather description

--- components/Weather.js
+++ components/Weather.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 function Weather({ data }) {
   console.log(data);
+  const temp = data.main.temp;
+  const roundedtemp = Math.round(temp * 10) / 10;
   return (
-    <div className='mt-20 flex flex-col justify-between items-center'>
+    <div className='mt-10 flex flex-col justify-between items-center'>
       <div>
         <h1 className='text-white text-2xl'>{data.name}</h1>
       </div>
       <div></div>
-      <div className='text-white text-3xl'>{data.main.temp}</div>
-      <div></div>
+      <div className='text-white text-4xl'>{roundedtemp} °C</div>
+      <div>
+        <h2 className='text-white'>{data.weather[0].main}</h2>
+      </div>
     </div>
   );
 }
