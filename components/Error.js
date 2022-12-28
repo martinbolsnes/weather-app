82a26250c5b0070c85f119ff82b32aa07de3296f
martinbolsnes <martinbolsnes@icloud.com>
2022-12-28 17:01:59 +0100

    create error comp

---
+++ components/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div className='w-full h-screen flex m-auto'>
+      <h2 className='text-white font-semibold m-auto'>
+        Oops, something went wrong. Please try again
+      </h2>
+    </div>
+  );
+}
+
+export default Error;
