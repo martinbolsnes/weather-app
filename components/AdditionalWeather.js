2797ad4b7aaca7cbbf5bd1fb7cbd6ba961a29d53
martinbolsnes <martinbolsnes@icloud.com>
2023-02-20 11:57:24 +0100

    add geoloaction

--- components/AdditionalWeather.js
+++ components/AdditionalWeather.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'weather-icons/css/weather-icons.css';
 
-function AdditionalWeather({ data }) {
-  const wind = data.wind.speed;
+function AdditionalWeather({ weatherData }) {
+  const wind = weatherData.wind.speed;
   const roundedwind = Math.round(wind * 10) / 10;
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center max-w-screen-sm m-auto'>
       <div className='flex justify-between w-4/5 md:w-2/5 items-center ml-4 mr-4 mt-6'>
         <div className='flex flex-col items-center align-center'>
           <i className='wi wi-strong-wind text-white text-2xl mb-2'></i>
@@ -14,12 +14,12 @@ function AdditionalWeather({ data }) {
         </div>
         <div className='flex flex-col items-center align-center'>
           <i className='wi wi-humidity text-white text-2xl mb-2 '></i>
-          <h3 className='text-white'>{data.main.humidity}%</h3>
+          <h3 className='text-white'>{weatherData.main.humidity}%</h3>
           <h4 className='text-white text-xs opacity-60'>Humidity</h4>
         </div>
         <div className='flex flex-col items-center align-center'>
           <i className='wi wi-barometer text-white text-2xl mb-2'></i>
-          <h3 className='text-white'>{data.main.pressure} hPa</h3>
+          <h3 className='text-white'>{weatherData.main.pressure} hPa</h3>
           <h4 className='text-white text-xs opacity-60'>Pressure</h4>
         </div>
       </div>
--- components/Weather.js
+++ components/Weather.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import 'weather-icons/css/weather-icons.css';
 
-function Weather({ data }) {
-  const temp = data.main.temp;
+function Weather({ weatherData }) {
+  const temp = weatherData.main.temp;
   const roundedtemp = Math.round(temp * 10) / 10;
 
   return (
     <div className='mt-10 flex flex-col justify-between items-center'>
       <div className='mb-10'>
-        <h1 className='text-white text-2xl font-semibold'>{data.name}</h1>
+        <h1 className='text-white text-2xl font-semibold'>
+          {weatherData.name}
+        </h1>
       </div>
       <div>
         <i
-          className={`wi wi-owm-${data.weather[0].id} text-9xl text-white`}
+          className={`wi wi-owm-${weatherData.weather[0].id} text-9xl text-white`}
         ></i>
       </div>
       <div className='text-white text-3xl font-bold mt-10'>
@@ -20,7 +22,7 @@ function Weather({ data }) {
       </div>
       <div>
         <h2 className='text-white font-semibold mt-6'>
-          {data.weather[0].main}
+          {weatherData.weather[0].main}
         </h2>
       </div>
     </div>
